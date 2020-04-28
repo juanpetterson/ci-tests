c065ee5df48cedcd2cf6e7a1a0ce6e600261c663
juanpetterson <juan.petterson@gmail.com>
2020-04-28 11:11:53 -0300

    Changed travis configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,15 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// Karma configuration
+// Generated on Thu Jul 28 2016 19:53:09 GMT-0700 (PDT)
 
 module.exports = function (config) {
   config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/netflix-angular'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true,
-    },
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
     // Config values to allow TravisCI to run chrome in it's container
     browsers: ['Chrome', 'ChromeCanary'],
     customLaunchers: {
@@ -27,14 +20,41 @@ module.exports = function (config) {
       },
     },
 
-    reporters: ['progress', 'kjhtml'],
+    // list of files / patterns to load in the browser
+    files: ['lib/*.js', 'test/*.js'],
+
+    // list of files to exclude
+    exclude: [],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {},
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+    // web server port
     port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-    restartOnFileChange: true,
   });
 
   // Detect if this is TravisCI running the tests and tell it to use chromium
